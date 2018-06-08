fce7e228391769bf989fcc2bbf8d7b576258ca3b
Josh Pollock <josh@calderawp.com>
2018-06-08 13:14:59 -0400

    add inlinde docs to module.js

--- src/state/module.js
+++ src/state/module.js
@@ -1,9 +1,12 @@
+/** Store**/
+//Actions
 import {
     setForms,
     setForm,
     addFormPreview
 } from './actions'
 
+//Selectors
 import {
     getForm,
     getForms,
@@ -11,17 +14,23 @@ import {
     getFormPreviews
 } from "./selectors";
 
+//Reducers
 import {
     formsReducer,
 } from "./reducers";
 
-
+/** State **/
 import {
     CALDERA_FORMS_STORE_SLUG,
     calderaFormsFormState,
     calderaFormsReduxStore
 } from "./index";
 
+/**
+ * All of the package in one export
+ *
+ * @type {{store: {actions: {setForms: setForms, setForm: setForm, addFormPreview: addFormPreview}, selectors: {getForm: getForm, getForms: getForms, getFormPreview: getFormPreview, getFormPreviews: getFormPreviews}, reducers: {formsReducer: formsReducer}}, state: {CALDERA_FORMS_STORE_SLUG: string, calderaFormsFormState: {reducer: formsReducer, actions: {setForms: setForms, setForm: setForm, addFormPreview: addFormPreview}, selectors: {getForm: getForm, getForms: getForms, getFormPreview: getFormPreview, getFormPreviews: getFormPreviews}}, calderaFormsReduxStore}}}
+ */
 module.exports  = {
     store : {
         actions: {
@@ -44,5 +53,4 @@ module.exports  = {
         calderaFormsFormState,
         calderaFormsReduxStore
     }
-
 };
